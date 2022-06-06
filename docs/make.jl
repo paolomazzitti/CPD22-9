f39c4dbaeb21b7025a1cbcaa3d180d62f2a7a169
Paolo Mazzitti <pao.mazzitti@stud.uniroma3.it>
2022-06-06 19:27:18 +0200

    test (2)

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,13 @@
-using Documenter, LARgenerators
+using Documenter, LARgenerators, DocumenterMarkdown
+using DocumenterTools: Themes
 
 makedocs(sitename="LARGenerators.jl",
     pages=[
-        "index.md",
-        "Page title" => "foo.md",
+        "Introduzione"=>"index.md",
+        "Grafo delle dipendenze" => "grafo.md",
+        "Sviluppo" => "sviluppo.md",
+        "Documentazione" => "docs.md",
+        "Analisi delle prestazioni" => "analisi.md",
     ],
     modules=[LARgenerators]
 )
@@ -11,3 +15,5 @@ makedocs(sitename="LARGenerators.jl",
 deploydocs(
     repo="github.com/paolomazzitti/CPD22-9.git",
 )
+
+Themes.compile("docs/src/documenter-light.scss", "docs/build/assets/themes/documenter-light.css")
